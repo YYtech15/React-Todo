a74a06c2d8bcf7fafa65a0a4c2b258d097d9a484
YYtech15 <midori1140flower@gmail.com>
2023-02-15 23:35:52 +0900

    準備

--- todo-app/src/index.js
+++ todo-app/src/index.js
@@ -1,14 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import App from "./App"
 
-reportWebVitals();
+ReactDOM.render(<App/>, document.getElementById("root"));
